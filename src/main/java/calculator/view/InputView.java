767b9281da09f44a4112da2693605292d3b68303
user <offonoff6101@naver.com>
2025-10-15 19:54:10 +0900

    feat(view): 입력 문구 출력 기능 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,13 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public String getInputNumber() {
+        System.out.println(INPUT_MESSAGE);
+        return Console.readLine();
+    }
 }
