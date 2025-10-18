4dd326e0035e7efa87090be7c044038ec97e2f4e
user <offonoff6101@naver.com>
2025-10-18 22:11:50 +0900

    feat(domain/Numbers): 필드 추가 및 합계 계산 메서드 구현

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -1,4 +1,21 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class Numbers {
+    private List<Integer> numbers;
+    private int numberSum;
+
+    public Numbers(List<Integer> numbers) {
+        this.numbers = numbers;
+        numberSum = sum(numbers);
+    }
+
+    private int sum(List<Integer> numbers) {
+        int result = 0;
+        for (Integer number : numbers) {
+            result += number;
+        }
+        return result;
+    }
 }
