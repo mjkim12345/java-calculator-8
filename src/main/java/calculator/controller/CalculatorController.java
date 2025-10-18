de4569d0a364e00bfee65e97259592d80eb70555
user <offonoff6101@naver.com>
2025-10-19 05:00:15 +0900

    feat(CalculatorController): 입력 처리 및 계산 실행 흐름 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,30 @@
 package calculator.controller;
 
+import calculator.domain.Numbers;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+import java.util.List;
+
 public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(InputView inputView, OutputView outputView, CalculatorService calculatorService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculatorService = calculatorService;
+    }
+
+    public void run() {
+        String input = inputView.input();
+        List<Integer> validatedNumbers = calculatorService.validateInputString(input);
+        Numbers numbers = new Numbers(validatedNumbers);
+        outputView.output(numbers.sum(validatedNumbers));
+    }
+
+
 }
